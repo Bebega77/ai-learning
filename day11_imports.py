a6b02e0b1f01042452d2290db85c8ba15cfcbd86
Bebega77 <b.bega80cr@gmail.com>
2026-01-12 16:39:22 +0500

    Day 11: libraries and import

---
+++ day11_imports.py
@@ -0,0 +1,10 @@
+# Day 11: libraries and import
+
+import math
+
+number = 24
+
+print("Квадратный корень:", math.sqrt(number))
+print("Округление вверх:", math.ceil(4.3))
+print("Округление вниз:", math.floor(4.7))
+print("Число π:", math.pi)
