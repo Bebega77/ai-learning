fb0a52c1c0525151f04696631702d4b23341424e
Bebega77 <b.bega80cr@gmail.com>
2026-01-12 15:01:03 +0500

    Day 5: lists basics

---
+++ day5_lists.py
@@ -0,0 +1,13 @@
+# Day 5: lists (списки)
+
+ages = [20, 30, 40, 50]
+
+print("Список возрастов:", ages)
+
+print("Первый возраст:", ages[0])
+print("Второй возраст:", ages[1])
+print("Последний возраст:", ages[-1])
+
+ages.append(60)
+ages.append(70)
+print("После добавления:", ages)
