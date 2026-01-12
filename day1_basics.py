e7935ef7e7b85eed62013540d2ac2fa4f4943c13
Bebega77 <b.bega80cr@gmail.com>
2026-01-12 10:45:12 +0500

    Day 1: first Python program

---
+++ day1_basics.py
@@ -0,0 +1,9 @@
+# Day 1: Python basics
+
+name = input("Введите ваше имя: ")
+age = int(input("Введите ваш возраст: "))
+
+years_to_100 = 100 - age
+
+print("Здравствуйте,", name)
+print("Через", years_to_100, "лет вам исполнится 100 лет")
